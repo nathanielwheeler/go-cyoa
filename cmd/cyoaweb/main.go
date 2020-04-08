781c0b36f3fbb45e0e000d5c9011455e8f588d9b
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-08 14:56:34 -0600

    Serves webpage

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"fmt"
 	"log"
 	"os"
 	"flag"
@@ -9,8 +11,10 @@ import (
 )
 
 func main() {
+	port := flag.Int("port", 3000, "CYOA starts on this port.")
 	filename := flag.String("file", "story.json", "JSON file with CYOA story")
 	flag.Parse()
+	fmt.Printf("Using story in %s\n", *filename)
 
 	file, err := os.Open(*filename)
 	if err != nil {
@@ -21,4 +25,8 @@ func main() {
 	if err != nil {
 		log.Fatalln("Error decoding story file.")
 	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Serving on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- cyoa.go
+++ cyoa.go
@@ -8,43 +8,6 @@ import (
 	"io"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.New("").Parse(""))
-}
-
-// NewHandler : Turns a story into an HTTP handler
-func NewHandler(s Story) http.Handler {
-	return handler{s}
-}
-
-type handler struct {
-	s Story
-}
-
-// TODO export into its own file
-// ServeHTTP : Method for handlers, takes in a writer and a request and serves a web page
-func (h handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(res, "index.html", nil)
-	if err != nil {
-		log.Fatalln("template didn't execute: ", err)
-	}
-	http.HandleFunc("/", index)
-	http.Handle("/web/", http.StripPrefix("/web", http.FileServer(http.Dir("web"))))
-	http.ListenAndServe(":8080", nil)
-}
-
-// JSONStory : Takes a reader, decodes from JSON into Story
-func JSONStory(reader io.Reader) (Story, error){
-	decoder := json.NewDecoder(reader)
-	var story Story
-	if err:= decoder.Decode(&story); err != nil {
-		return nil, err
-	}
-	return story, nil
-}
-
 // Story : Holds a bunch of chapters
 type Story map[string]Chapter
 
@@ -60,3 +23,38 @@ type Option struct {
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
 }
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(""))
+}
+
+// NewHandler : Turns a story into an HTTP handler
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+// ServeHTTP : Method for handlers, takes in a writer and a request and serves a web page
+func (h handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(res, h.s["intro"])
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}
+
+// JSONStory : Takes a reader, decodes from JSON into Story
+func JSONStory(reader io.Reader) (Story, error){
+	decoder := json.NewDecoder(reader)
+	var story Story
+	if err:= decoder.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+
