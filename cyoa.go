cfe4d463c4d75b83626decc1b8eaf9667f5052e6
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-10 13:40:52 -0600

    Using template files instead of an inline template

--- cyoa.go
+++ cyoa.go
@@ -27,55 +27,12 @@ type Option struct {
 
 var tpl *template.Template
 
-// TODO switch to templates
-var handlerTemplate = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" href="style.css">
-	<title>¡CYOA!</title>
-</head>
-
-<body>
-	<header>
-		<!-- Chapter title goes here -->
-		<h1 class="text-center">{{.Title}}</h1>
-	</header>
-	<main class="container">
-		<div class="row">
-			<!-- Paragraphs go here -->
-			{{range .Paragraphs}}
-			<p class="col-12">
-				{{.}}
-			</p>
-			{{end}}
-		</div>
-		<div class="row">
-			<!-- Options go here -->
-			{{range .Options}}
-			<div class="col">
-				<button class="btn btn-outline-danger btn-lg btn-block"
-					onclick="window.location.href = '/{{.Arc}}';">{{.Text}}</button>
-			</div>
-			{{end}}
-		</div>
-	</main>
-</body>
-
-</html>
-`
-
 type handler struct {
 	s Story
 }
 
 func init() {
-	tpl = template.Must(template.New("").Parse(handlerTemplate))
+	tpl = template.Must(template.ParseFiles("web/index.html"))
 }
 
 // NewHandler : Turns a story into an HTTP handler
@@ -87,7 +44,7 @@ func NewHandler(s Story) http.Handler {
 // ServeHTTP : Method for handlers, takes in a writer and a request and serves a web page
 func (h handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Executing story...")
-	err := tpl.Execute(res, h.s["intro"])
+	err := tpl.ExecuteTemplate(res, "index.html", h.s["intro"])
 	if err != nil {
 		log.Fatalln("template didn't execute: ", err)
 	}
